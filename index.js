1f7dcc8218992c2b11f85938493ddabe1df91265
Jeff Jewiss <jeffjewiss@gmail.com>
2016-03-08 13:56:43 -0500

    Makes plugin easier to demo

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
-var postcss = require('postcss');
-var random = require("random-js")();
+var postcss = require('postcss')
+var random = require('random-js')()
 
 module.exports = postcss.plugin('postcss-randomize', function (opts) {
-  var randName = 'randomize'
-  var propsName = 'rand-props'
-
   /*
      @randomize example 1-5
      @rand-props color blue,white,red
   */
 
-
-
   return function (css) {
     css.walk(function (target) {
 
-      if (isAtRule(target) && target.name === randName) {
+      /* Breakout: introduce devtool here */
+      if (target.type === 'atrule' && target.name === 'randomize') {
         var params = target.params.split(' ')
         var className = params[0]
         var range = params[1].split('-')
-        var min = parseInt(range[0])
-        var max = parseInt(range[1])
+        var min = parseInt(range[0], 10)
+        var max = parseInt(range[1], 10)
         var randomRules = []
 
         for (var i = min; i !== max + 1; i++) {
-          randomRules.push(postcss.rule({
+          var newRule = postcss.rule({
             selector: '.' + className + '-' + i,
             nodes: target.nodes
-          }))
+          })
+
+          randomRules.push(newRule)
         }
 
-        randomRules.forEach(function(newNode) {
-          newNode.nodes = newNode.nodes.map(function(childNode) {
-            if (!(isAtRule(childNode) && childNode.name === propsName)) {
-              return childNode;
+        // debugger
+        // use a debugger to show the randomRules array and nodes in devtool
+
+        randomRules.forEach(function (randomRule) {
+          randomRule.nodes = randomRule.nodes.map(function (childNode) {
+            if (!(childNode.type === 'atrule' && childNode.name === 'rand-props')) {
+              return childNode
             }
 
             var params = childNode.params.split(' ')
@@ -47,8 +48,14 @@ module.exports = postcss.plugin('postcss-randomize', function (opts) {
           })
         })
 
+        // debugger
+        // use a debugger to show the child nodes in devtool to show that the randomizer
+        // is working as intended
+
         target.replaceWith(randomRules)
       }
+
+      // Refresh devtool a bunch, prove it's random!
 
     })
   }
@@ -56,8 +63,4 @@ module.exports = postcss.plugin('postcss-randomize', function (opts) {
 
 function randomValue (values) {
   return values[random.integer(0, values.length - 1)]
-}
-
-function isAtRule (node) {
-  return node.type === 'atrule'
 }
